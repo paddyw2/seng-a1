11db0e57aea4ba97aa6e14d566eefb5441898c5d
Paddy <p.withams@gmail.com>
2017-01-17 18:06:54 -0700

    Add VendingMachine class

---
+++ seng301-asgn1/seng301-asgn1/src/Frontend1/VendingMachine.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace FrontEnd1
+{
+    public class VendingMachine
+    {
+        public VendingMachine()
+        {
+        }
+
+        public VendingMachine(int id, List<int> listOfCoins, int buttonCount)
+        {
+        }
+    }
+}
+
